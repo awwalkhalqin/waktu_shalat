23cc5e66a0a5e988d35b2b016a7ea1bedd582b60
awwalkhalqin <awwala1012@gmail.com>
2020-10-12 07:34:42 +0700

    waktu_shalat

---
+++ android/app/src/main/kotlin/com/example/waktu_shalat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.waktu_shalat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/model/results.dart
@@ -0,0 +1,24 @@
+import 'package:waktu_shalat/model/datetime.dart';
+import 'package:waktu_shalat/model/location.dart';
+import 'package:waktu_shalat/model/settings.dart';
+
+class Results {
+  List<Datetime> datetime;
+  Location location;
+  Settings settings;
+
+  Results.fromJsonMap(Map<String, dynamic> map)
+      : datetime = List<Datetime>.from(
+            map["datetime"].map((it) => Datetime.fromJsonMap(it))),
+        location = Location.fromJsonMap(map["location"]),
+        settings = Settings.fromJsonMap(map["settings"]);
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['datetime'] =
+        datetime != null ? this.datetime.map((v) => v.toJson()).toList() : null;
+    data['location'] = location == null ? null : location.toJson();
+    data['settings'] = settings == null ? null : settings.toJson();
+    return data;
+  }
+}
